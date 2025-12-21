45b3fd70afb9654b95dcb6ee70a5f2e55713769e
abydow <Okamiwolfie@outlook.com>
2025-12-22 00:23:59 +0530

    addition of database

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,8 @@
 - License
 - README.md
 - .gitignore
+
+## 21st Dec
+
+- Added the sqlite package
+- created a database table to store the data of user and guild and a integer slot for storing warnings issued.
--- src/bot/main.py
+++ src/bot/main.py
@@ -3,6 +3,25 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import sqlite3
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def create_user_table():
+    connection = sqlite3.connect(f'{BASE_DIR}/user_warnings.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+          CREATE TABLE IF NOT EXISTS "users_per_guild" (
+          "user_id" INTEGER,
+          "warning_count" INTEGER,
+          "guild_id" INTEGER,
+          PRIMARY KEY ("user_id", "guild_id")
+          )
+    ''')
+    connection.commit()
+    connection.close()
+
+create_user_table()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,7 +34,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    print(f'{bot.user} has connected to Discord!')
+    print('╭────────────────────────────────╮')
+    print('│ SYNC has connected to Discord! │')
+    print('╰────────────────────────────────╯')
 
 @bot.event
 async def on_message(msg):
